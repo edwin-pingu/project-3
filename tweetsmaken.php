af6e70dc99c7d9a41c0986bfdc28cb41eadaab2c
Edwin <edwin15vaneijk@gmail.com>
2024-04-12 23:59:03 +0200

    test maken tweets

--- tweetsmaken.php
+++ tweetsmaken.php
@@ -6,6 +6,34 @@
     <title>Tweets</title>
 </head>
 <body>
-    
+<?php
+    try {
+        $db = new PDO ("mysql:host=localhost;dbname=tweets", "root", "");
+        if (isset($_POST['verzenden'])) {
+            $tweet = filter_input(INPUT_POST, "tweet", FILTER_SANITIZE_STRING);
+
+
+            $query = $db->prepare("INSERT INTO tweetsshow (tweets) VALUES (:tweets)");
+
+            $query->bindParam("tweet", $tweet);
+
+            if($query->execute()) {
+                echo "De nieuwe gevens zijn toegevoegd";
+            } else {
+                echo "Er is een fout opgetreden";
+            }
+            echo "<br>";
+        }
+    }
+    catch(PDOException $e) {
+        die("Error!: " . $e->getMessage());
+    }
+    ?>
+
+        <form action="tweetmaken.php" method="post">
+        <label for="merk">Maak u tweet</label>
+        <input type="text" name="tweet" id="tweet">
+        <br>
+        <input type="submit" name="verzenden" value="Verzenden">
 </body>
 </html>

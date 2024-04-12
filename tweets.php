511bc5b839f9b65ae40047e0fb6241252dd3ae50
Edwin <edwin15vaneijk@gmail.com>
2024-04-12 23:44:04 +0200

    tweets.php

---
+++ tweets.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tweets</title>
+</head>
+<body>
+<?php
+$servername = "localhost";
+$tweets = "root";
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=tweets", $tweets);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
+</body>
+</html>
